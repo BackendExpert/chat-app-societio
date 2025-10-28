51f0cd04371657e64e606ec60705068e51d05210
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-28 20:03:45 +0530

    Update

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '../pages/HomePage/HomePage'
+import DefultError from '../component/Errors/DefultError'
+import WebSite from '../layouts/WebSite'
+
 
 function App() {
 
     return (
         <BrowserRouter>
             <Routes>
-
+                <Route path='/' element={<WebSite />}>
+                    <Route path="*" element={<DefultError /> } />
+                    <Route index element={<HomePage /> } />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
